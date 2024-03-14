fa29ab2480584692ac6681123a1efeceb68d5f2e
Stephen Moloney <stephen@stephenmoloney.com>
2024-03-14 14:29:46 +0000

    Added: missings quotes in azure-pipelines.yaml

--- .ci/functions_init.sh
+++ .ci/functions_init.sh
@@ -7,16 +7,24 @@ function install_asdf() {
     export ASDF_DIR="${HOME}/.asdf" && source "${HOME}/.asdf/asdf.sh"
 }
 
+function install_shfmt() {
+    wget -O - https://raw.githubusercontent.com/stephenmoloney/localbox/master/bin/install/shfmt.sh | bash
+}
+
 function install_node() {
+    local node_version="${1:-20.11.1}"
+
     asdf plugin add nodejs https://github.com/asdf-vm/asdf-nodejs.git
-    asdf install nodejs 16.14.2
-    asdf global nodejs 16.14.2
+    asdf install nodejs "${node_version}"
+    asdf global nodejs "${node_version}"
 }
 
 function install_yarn() {
+    local yarn_version="${1:-1.22.19}"
+
     asdf plugin add yarn
-    asdf install yarn 1.22.18
-    asdf global yarn 1.22.18
+    asdf install yarn "${yarn_version}"
+    asdf global yarn "${yarn_version}"
 }
 
 function install_npm_modules() {
@@ -24,12 +32,15 @@ function install_npm_modules() {
 }
 
 function install_opentofu() {
+    local opentofu_version="${1:-1.6.2}"
+
     asdf plugin add opentofu https://github.com/virtualroot/asdf-opentofu.git
-    asdf install opentofu latest
-    asdf global opentofu latest
+    asdf install opentofu "${opentofu_version}"
+    asdf global opentofu "${opentofu_version}"
 }
 
 function install_all_deps() {
+    install_shfmt
     install_asdf &&
         (
             install_node
