ac505b4f0d028f4804a06fea0d5d50ba01b7ecb6
Stephen Moloney <12668653+stephenmoloney@users.noreply.github.com>
2024-03-20 21:53:39 +0000

    Fixed: incorrect provider name (#9)
    ***What does this change do?***
    
    - The name of the scaleway provider was incorrect
    
    ***Why is this change needed?***
    
    - Fix the scw-ssh-keys module

--- modules/scw-ssh-key/versions.tf
+++ modules/scw-ssh-key/versions.tf
@@ -2,7 +2,7 @@ terraform {
   required_version = ">= 1.0, < 2.0"
 
   required_providers {
-    azurerm = {
+    scaleway = {
       source  = "registry.opentofu.org/scaleway/scaleway"
       version = "2.38.2"
     }
